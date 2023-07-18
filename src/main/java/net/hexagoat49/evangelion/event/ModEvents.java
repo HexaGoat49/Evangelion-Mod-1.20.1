0952ccd785d52053b6cf56b964e03bde7c647a81
HexaGoat49 <hexalg49@gmail.com>
2023-07-17 23:32:42 -0400

    Added player jump packet Added event for jumping

--- src/main/java/net/hexagoat49/evangelion/event/ModEvents.java
+++ src/main/java/net/hexagoat49/evangelion/event/ModEvents.java
@@ -1,6 +1,10 @@
 package net.hexagoat49.evangelion.event;
 
 import net.hexagoat49.evangelion.Evangelion;
+import net.hexagoat49.evangelion.networking.ModMessages;
+import net.hexagoat49.evangelion.networking.packet.PlayerJumpC2SPacket;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -8,6 +12,12 @@ import net.minecraftforge.fml.common.Mod;
 public class ModEvents {
     @Mod.EventBusSubscriber(modid = Evangelion.MOD_ID)
     public static class ForgeEvents {
+        @SubscribeEvent
+        public static void onPlayerJump(InputEvent event) {
+            if(Minecraft.getInstance().options.keyJump.isDown()) {
+                ModMessages.sendToServer(new PlayerJumpC2SPacket());
+            }
+        }
     }
 
     @Mod.EventBusSubscriber(modid = Evangelion.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
---
+++ src/main/java/net/hexagoat49/evangelion/networking/packet/PlayerJumpC2SPacket.java
@@ -0,0 +1,30 @@
+package net.hexagoat49.evangelion.networking.packet;
+
+import net.hexagoat49.evangelion.entity.custom.Eva01;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.function.Supplier;
+
+public class PlayerJumpC2SPacket {
+    public PlayerJumpC2SPacket() {}
+
+    public PlayerJumpC2SPacket(FriendlyByteBuf buf) {}
+
+    public void toBytes(FriendlyByteBuf buf) {}
+
+    public boolean handle(Supplier<NetworkEvent.Context> supplier) {
+        NetworkEvent.Context context = supplier.get();
+        context.enqueueWork(() -> {
+            ServerPlayer player = context.getSender();
+
+            // Make the Eva jump
+            if(player.getVehicle() instanceof Eva01) {
+                ((Eva01) player.getVehicle()).doJump();
+            }
+        });
+        return true;
+    }
+
+}
